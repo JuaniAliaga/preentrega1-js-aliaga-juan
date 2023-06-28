e16d8319f5924038f9730700ea1aaab648b3501d
Juan Aliaga <juanialiaga4@gmail.com>
2023-06-27 22:13:09 -0300

    agregado de factura de compra y anidado de codigo en funcion

--- main.js
+++ main.js
@@ -16,37 +16,42 @@ let agregar;
 
 alert("Bienvenido a nuestro carrito, presione aceptar para comenzar");
 
-do {
-    const listaStringProductos = productos.map(
-        (product, index) => `
+function ejecucionDePrograma() {
+
+    do {
+        const listaStringProductos = productos.map(
+            (product, index) => `
                     ${index + 1}: ${product.nombre} precio: ${product.precio}
 `);
-    opcion = parseInt(
-        prompt("Que producto desea elegir?" + "\n" + listaStringProductos.join('\n')));
-
-    while (opcion <= 0 || opcion > productos.length || isNaN(opcion)) {
         opcion = parseInt(
-            prompt("Ingrese una opción válida" + "\n" + listaStringProductos.join('\n')));
-    }
+            prompt("Que producto desea elegir?" + "\n" + listaStringProductos.join('\n')));
 
-    cantidad = parseInt(prompt("Ingrese la cantidad que desea"));
+        while (opcion <= 0 || opcion > productos.length || isNaN(opcion)) {
+            opcion = parseInt(
+                prompt("Ingrese una opción válida" + "\n" + listaStringProductos.join('\n')));
+        }
 
-    while (cantidad <= 0 || isNaN(cantidad)) {
-        cantidad = parseInt(prompt("Ingrese una cantidad valida"));
-    }
+        cantidad = parseInt(prompt("Ingrese la cantidad que desea"));
 
-    detalleFactura.push({
-        nombre: productos[opcion - 1].nombre,
-        precioUnitario: productos[opcion - 1].precio,
-        cantidad,
-        subtotal: productos[opcion - 1].precio * cantidad
-    });
+        while (cantidad <= 0 || isNaN(cantidad)) {
+            cantidad = parseInt(prompt("Ingrese una cantidad valida"));
+        }
 
-    agregar = prompt("Deseas agregar mas productos al carrito? si/no").toLowerCase();
+        detalleFactura.push({
+            nombre: productos[opcion - 1].nombre,
+            precioUnitario: productos[opcion - 1].precio,
+            cantidad,
+            subtotal: productos[opcion - 1].precio * cantidad
+        });
 
-} while (agregar == "si")
+        agregar = prompt("Deseas agregar mas productos al carrito? si/no").toLowerCase();
 
+    } while (agregar == "si")
 
+    total = detalleFactura.reduce((acc, itemFactura) => acc + itemFactura.precioUnitario * itemFactura.cantidad, 0);
 
+    const detalleFacturaString = detalleFactura.map((itemFactura) =>
+        `Producto: ${itemFactura.nombre}, Cantidad: ${itemFactura.cantidad}, Subtotal: $${itemFactura.subtotal}`);
 
-alert("El total a pagar de su compra es de: $" + total)
+    alert("El total a pagar de su compra es de: $" + total + "\n" + "\n" + detalleFacturaString.join('\n'));
+}
